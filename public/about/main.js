d66b6930b35b49e533c26f96ece7dc14bdbbe541
Flaschon <winsleytan2008@gmail.com>
2025-02-20 23:40:19 +0800

    dev2.2.2

---
+++ public/about/main.js
@@ -0,0 +1,52 @@
+document.getElementById('contact').addEventListener('click', function() {
+    var redirectWindow = window.open('https://discord.com/', '_blank');
+    redirectWindow.location;
+  });
+
+
+  //Button Hyperlink
+document.getElementById('home').addEventListener('click', function() {
+    window.location.href = '../backToHome.html'; 
+  });
+  
+  document.getElementById('register-page').addEventListener('click', function() {
+    window.location.href = '../projects/register.html'; 
+  });
+  
+  document.getElementById('login-page').addEventListener('click', function() {
+    window.location.href = '../projects/login/index.html'; 
+  });
+  
+  document.getElementById('online-ordering-page').addEventListener('click', function() {
+    window.location.href = '../projects/ordering-homepage.html';
+  });
+  
+  document.getElementById('tictactoe').addEventListener('click', function() {
+    window.location.href = '../games/tictactoe/tictactoe.html'; 
+  });
+  
+  document.getElementById('rockPaperScissors').addEventListener('click', function() {
+    window.location.href = '../games/rockPaperScissors/index.html'; 
+  });
+  
+  document.getElementById('tower').addEventListener('click', function() {
+    window.location.href = '../games/towerBlocks/index.html'; 
+  });
+  
+  
+  document.getElementById('about').addEventListener('click', function() {
+    window.location.href = './'; 
+  });
+  
+  document.getElementById('feedback').addEventListener('click', function() {
+    var redirectWindow = window.open('https://docs.google.com/forms/d/e/1FAIpQLSdNS69oTTUbBuol9aEz7nttJ9MZeRUQ5pyRchDSsVDUV9YWIw/viewform', '_blank');
+    redirectWindow.location;
+  });
+  
+  document.getElementById('patch').addEventListener('click', function() {
+    window.location.href = '../patchNotes/index.html'; 
+  });
+  
+  document.getElementById('post').addEventListener('click', function() {
+    window.location.href = '../post/index.html'; 
+  });
--- public/defaultMain.js
+++ public/defaultMain.js
@@ -1,3 +1,52 @@
+// //Button Hyperlink
+// document.getElementById('home').addEventListener('click', function() {
+//   window.location.href = '../backToHome.html'; 
+// });
+
+// document.getElementById('register-page').addEventListener('click', function() {
+//   window.location.href = '../projects/register.html'; 
+// });
+
+// document.getElementById('login-page').addEventListener('click', function() {
+//   window.location.href = '../projects/login/index.html'; 
+// });
+
+// document.getElementById('online-ordering-page').addEventListener('click', function() {
+//   window.location.href = '../projects/ordering-homepage.html';
+// });
+
+// document.getElementById('tictactoe').addEventListener('click', function() {
+//   window.location.href = '../games/tictactoe/tictactoe.html'; 
+// });
+
+// document.getElementById('rockPaperScissors').addEventListener('click', function() {
+//   window.location.href = '../games/rockPaperScissors/index.html'; 
+// });
+
+// document.getElementById('tower').addEventListener('click', function() {
+//   window.location.href = '../games/towerBlocks/index.html'; 
+// });
+
+
+// document.getElementById('about').addEventListener('click', function() {
+//   window.location.href = '../about/index.html'; 
+// });
+
+// document.getElementById('feedback').addEventListener('click', function() {
+//   var redirectWindow = window.open('https://docs.google.com/forms/d/e/1FAIpQLSdNS69oTTUbBuol9aEz7nttJ9MZeRUQ5pyRchDSsVDUV9YWIw/viewform', '_blank');
+//   redirectWindow.location;
+// });
+
+// document.getElementById('patch').addEventListener('click', function() {
+//   window.location.href = '../patchNotes/index.html'; 
+// });
+
+// document.getElementById('post').addEventListener('click', function() {
+//   window.location.href = '../post/index.html'; 
+// });
+
+
+
 const subMenus = document.querySelectorAll(".sub-menu"),
   buttons = document.querySelectorAll(".sidebar ul button");
 
@@ -22,52 +71,6 @@ const onClick = (item) => {
   }
 };
 
-//Button Hyperlink
-document.getElementById('home').addEventListener('click', function() {
-  window.location.href = './'; 
-});
-
-document.getElementById('post').addEventListener('click', function() {
-  window.location.href = './public/post/index.html'; 
-});
-
-document.getElementById('register-page').addEventListener('click', function() {
-  window.location.href = './public/projects/register.html'; 
-});
-
-document.getElementById('login-page').addEventListener('click', function() {
-  window.location.href = './public/projects/login/index.html'; 
-});
-
-document.getElementById('online-ordering-page').addEventListener('click', function() {
-  window.location.href = './public/projects/ordering-homepage.html';
-});
-
-document.getElementById('tictactoe').addEventListener('click', function() {
-  window.location.href = './public/games/tictactoe/tictactoe.html'; 
-});
-
-document.getElementById('rockPaperScissors').addEventListener('click', function() {
-  window.location.href = './public/games/rockPaperScissors/index.html'; 
-});
-
-document.getElementById('tower').addEventListener('click', function() {
-  window.location.href = './public//games/towerBlocks/index.html'; 
-});
-
-
-document.getElementById('about').addEventListener('click', function() {
-  window.location.href = './public/about/index.html'; 
-});
-
-document.getElementById('feedback').addEventListener('click', function() {
-  var redirectWindow = window.open('https://docs.google.com/forms/d/e/1FAIpQLSdNS69oTTUbBuol9aEz7nttJ9MZeRUQ5pyRchDSsVDUV9YWIw/viewform', '_blank');
-  redirectWindow.location;
-});
-
-document.getElementById('patch').addEventListener('click', function() {
-  window.location.href = './public/patchNotes/index.html'; 
-});
 
 
 function toggleSidebar() {
